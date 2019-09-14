cda0a1b79423f73707e7c30356aa11934625983b
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-09-15 00:30:16 +0300

    :D

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const electron = require('electron')
+const _ = require('lodash')
 
 require('electron-reload')(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`)
 })
 const path = require('path')
 
-let mainWindow
+const windows = []
+
+function closeAll() {
+  console.log('Closing all!')
+  windows.forEach(window => {
+    if (window) {
+      window.close()
+    }
+  })
+}
 
 function createWindows() {
-
+  let mainWindow
   const allScreens = electron.screen.getAllDisplays()
   allScreens.forEach(screen => {
-    console.log(screen)
+    // console.log(screen)
     mainWindow = new electron.BrowserWindow({
       width: screen.size.width,
       height: screen.size.height,
@@ -22,16 +32,23 @@ function createWindows() {
       }
     })
     mainWindow.loadFile('index.html')
-    mainWindow.on('closed', function() {
-      mainWindow = null
+    mainWindow.on('closed', () => {
+      // closeAll()
     })
+    mainWindow.maximize()
+    windows.push(mainWindow)
   })
 }
 
 electron.app.on('ready', createWindows)
-electron.app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') electron.app.quit()
+
+electron.app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    electron.app.quit()
+  }
 })
 electron.app.on('activate', function() {
-  if (mainWindow === null) createWindows()
+  if (mainWindow === null) {
+    createWindows()
+  }
 })
