3d962cb39458363a527f56d94eb433934c3440c8
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-09-15 00:54:04 +0300

    >:|

--- main.js
+++ main.js
@@ -8,27 +8,27 @@ const path = require('path')
 const windows = []
 
 function createWindows() {
-  let mainWindow
   const allScreens = electron.screen.getAllDisplays()
   allScreens.forEach(screen => {
-    // console.log(screen)
-    mainWindow = new electron.BrowserWindow({
+    console.log(screen)
+    const mainWindow = new electron.BrowserWindow({
       width: screen.size.width,
       height: screen.size.height,
-      x: screen.workArea.x,
-      y: screen.workArea.y,
+      x: screen.bounds.x,
+      y: screen.bounds.y,
       webPreferences: {
         preload: path.join(__dirname, 'preload.js')
       },
-      // show: false,
       movable: false,
       resizable: false,
       alwaysOnTop: false,
       fullscreen: true,
+      useContentSize: true,
     })
 
     mainWindow.loadFile('index.html')
     mainWindow.on('closed', () => {
+      // Close one window, close everything.
       electron.app.quit()
     })
     // mainWindow.maximize()
