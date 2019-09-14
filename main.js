ae9eca8fac37a9251d93f2a7364d81131a46212d
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-09-15 00:14:46 +0300

    All screens are filled.

--- main.js
+++ main.js
@@ -7,31 +7,31 @@ const path = require('path')
 
 let mainWindow
 
-function createWindow() {
+function createWindows() {
 
-  const screenElectron = electron.screen
-  const allScreens = screenElectron.getAllDisplays()
-  console.log(allScreens)
-
-  mainWindow = new electron.BrowserWindow({
-    width: 7680,
-    height: 8000,
-    x: -1560,
-    y: -40,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  mainWindow.loadFile('index.html')
-  mainWindow.on('closed', function() {
-    mainWindow = null
+  const allScreens = electron.screen.getAllDisplays()
+  allScreens.forEach(screen => {
+    console.log(screen)
+    mainWindow = new electron.BrowserWindow({
+      width: screen.size.width,
+      height: screen.size.height,
+      x: screen.workArea.x,
+      y: screen.workArea.y,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+    mainWindow.loadFile('index.html')
+    mainWindow.on('closed', function() {
+      mainWindow = null
+    })
   })
 }
 
-electron.app.on('ready', createWindow)
+electron.app.on('ready', createWindows)
 electron.app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') electron.app.quit()
 })
 electron.app.on('activate', function() {
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null) createWindows()
 })
